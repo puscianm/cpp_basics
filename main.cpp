ebfcff2d52f0e0af8b1351fadf3fb1e28a564207
puscianm <teranowa1995@gmail.com>
2024-04-22 20:41:31 +0200

    initial commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    for (int i=1; i <= 20; i++)
+    {
+        std::cout << i <<std::endl;
+    }
+    return 0;
+}
