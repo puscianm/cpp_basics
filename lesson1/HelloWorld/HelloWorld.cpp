8b5c9e4b2deaf393c01ad3cc32a652eb2f40c7d3
puscianm <teranowa1995@gmail.com>
2024-04-23 23:08:01 +0200

    added functions representing chapters 1.3-1.4

--- lesson1/HelloWorld/HelloWorld.cpp
+++ lesson1/HelloWorld/HelloWorld.cpp
@@ -1,7 +1,26 @@
 ﻿#include <iostream>
 
+void variable_initialization() {
+	// list initialization - preferred method in modern C++
+	int x{ 4 };
+
+	std::cout << "initialized variable (object with identifier[name]) has value " << x << std::endl;
+
+	// value initialization (initializes variable with 0 or empty)
+	int y{};
+
+	y = 10;
+}
+
+void maybe_unused_variables() {
+	// telling compiler that these variables can be unused
+	[[maybe_unused]] double pi{ 3.14 };
+}
+
+
 int main()
 {
-	std::cout << "Hello world!";
+	variable_initialization();
+	maybe_unused_variables();
 	return 0;
 }
