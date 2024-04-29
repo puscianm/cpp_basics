91f04af7b4ece0ffd7f25b837a3c302fe6cba1be
puscianm <teranowa1995@gmail.com>
2024-04-29 18:02:59 +0200

    changed HelloWorld to chapter1 and added ls1.5

---
+++ chapter_1/chapter_1/chapter1.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+
+void lesson_1_4_variable_initialization() {
+	std::cout << "--------lesson 1.4--------" << '\n';
+
+	// list initialization - preferred method in modern C++
+	int x{ 4 };
+
+	std::cout << "initialized variable (object with identifier[name]) has value " << x << '\n';
+
+	// value initialization (initializes variable with 0 or empty)
+	int y{};
+
+	y = 10;
+
+	// telling compiler that these variables can be unused
+	[[maybe_unused]] double pi{ 3.14 };
+
+}
+
+void lesson_1_5_cout_cin() {
+	std::cout << "--------lesson 1.5--------" << '\n';
+
+	std::cout << "This string is passed to the cout variable" << std::endl;
+	std::cout << "Outputing cout to console typically happens in batches." << '\n';
+	std::cout << "It's better to use backslash n than endl" << "\n \n";
+
+	std::cout << "Please enter a number and press enter" << '\n';
+	int user_varaible{};
+	std::cin >> user_varaible;
+	std::cout << "User inputed: " << user_varaible << '\n';
+
+}
+
+void lesson_1_6_uninitialized_variables() {
+	int unitialized_variable;
+	std::cout << unitialized_variable;
+}
+
+
+int main()
+{
+	lesson_1_4_variable_initialization();
+	//lesson_1_5_cout_cin();
+	lesson_1_6_uninitialized_variables();
+	return 0;
+}
