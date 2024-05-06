eebf30cccf6536b5aba5e9cfe2e392e2d2c08679
puscianm <teranowa1995@gmail.com>
2024-05-06 23:04:22 +0200

    added chapter 2

--- chapter_1/chapter_1/chapter1.cpp
+++ chapter_1/chapter_1/chapter1.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 
-void lesson_1_4_variable_initialization() {
+void lesson_1_4_variable_initialization() 
+{
 	std::cout << "--------lesson 1.4--------" << '\n';
 
 	// list initialization - preferred method in modern C++
@@ -18,7 +19,8 @@ void lesson_1_4_variable_initialization() {
 
 }
 
-void lesson_1_5_cout_cin() {
+void lesson_1_5_cout_cin() 
+{
 	std::cout << "--------lesson 1.5--------" << '\n';
 
 	std::cout << "This string is passed to the cout variable" << std::endl;
@@ -32,13 +34,15 @@ void lesson_1_5_cout_cin() {
 
 }
 
-void lesson_1_6_uninitialized_variables() {
+void lesson_1_6_uninitialized_variables() 
+{
 	std::cout << "--------lesson 1.6--------" << '\n';
 	//int unitialized_variable;
 	//std::cout << unitialized_variable;
 }
 
-void lesson17KewordAndNamingIndentifiers() {
+void lesson17KewordAndNamingIndentifiers() 
+{
 	std::cout << "--------lesson 1.7--------" << '\n';
 
 	//we use small letters for variables and functions
@@ -46,13 +50,52 @@ void lesson17KewordAndNamingIndentifiers() {
 
 	//good variable and function names
 	int coins{};
-	std::cout << coins;
+	std::cout << coins << '\n';
 	int coinsCount();
 
 	//good practice is to describe in comment above what variable is used for. E.g.
 
 	// Length of boat in centimeters. Measured from bow to stern.
-	int boatLength {};
+	[[maybe_unused]] int boatLength {};
+}
+
+void lesson19() 
+{
+	std::cout << "--------lesson1.9--------" << '\n';
+
+	[[maybe_unused]] int x { 5 }; // x is variable, 5 is literal
+
+
+	// These lines do completly different things
+	std::cout << 5 << '\n';
+	std::cout << x << '\n';
+
+	// When compiler sees first line, it will create executable that prints value 5 into console
+	// When compiler sees second line, it will create executable that fetches value from memory assigned to x and then print it.
+	
+
+	std::cout << 5 + 7 << '\n'; //This is operator+. It is binary, meaning it takes two arguments.
+
+	//An operator (or function) that has some observable effect beyond producing a return value is said to have a side effect. 
+	// For example, when x = 5 is evaluated, the assignment operator has the side effect of assigning the value 5 to variable x.
+}
+
+void summaryChapter()
+{
+	std::cout << "--------summaryChapter--------" << '\n';
+
+	std::cout << "Enter an integer: ";
+
+	int userInputFirst{ };
+	std::cin >> userInputFirst;
+
+	std::cout << "Enter another integer: ";
+	
+	int userInputSecond{ };
+	std::cin >> userInputSecond;
+
+	std::cout << userInputFirst << " + " << userInputSecond << " is " << userInputFirst + userInputSecond << ".\n";
+	std::cout << userInputFirst << " - " << userInputSecond << " is " << userInputFirst - userInputSecond << ".\n";
 }
 
 
@@ -62,5 +105,6 @@ int main()
 	//lesson_1_5_cout_cin();
 	lesson_1_6_uninitialized_variables();
 	lesson17KewordAndNamingIndentifiers();
+	summaryChapter();
 	return 0;
 }
