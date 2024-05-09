956d4bd9fa778304e034fa81315a2c7ec80a881e
puscianm <teranowa1995@gmail.com>
2024-05-09 21:02:27 +0200

    added lesson about local scope

--- chapter_2/chapter2/chapter2.cpp
+++ chapter_2/chapter2/chapter2.cpp
@@ -15,7 +15,24 @@ int getUserInput()
     return userInput;
 }
 
+void function_inside_main() {
+
+    int x{ 5 };
+    int y{ 6 };
+
+    std::cout << "function_inside_main has no idea about varaibles x and y from main. x = " << x << " y = " << y << '\n';
+}
+
 int main()
 {
-    std::cout << "Doubled user input is: " << doubleNumber(getUserInput());
+    //std::cout << "Doubled user input is: " << doubleNumber(getUserInput());
+
+    int x { 2 };
+    int y { 4 };
+
+    std::cout << "x and y are here from main. x = " << x << " y = " << y << '\n';
+
+    function_inside_main();
+
+    std::cout << "And we are back inside main. x = " << x << " y = " << y << '\n';
 }
