1839baa3a5ddb9f053f7fe90f567745e3532ab40
puscianm <teranowa1995@gmail.com>
2024-05-11 23:09:14 +0200

    added lesson about header files and header guards

---
+++ fundamentalDataTypes/master.cpp
@@ -0,0 +1,13 @@
+int main()
+{
+    /*
+    Smallest unit of memory
+        bit (1 or 0)
+    Than we have
+        byte a goup of bits that are operated on as a unit
+        modern standard is that byte is a group of 8 bits
+    Memory adress
+        holds one byte of data
+    */
+    return 0;
+}
---
+++ headerGuards/master.h
@@ -0,0 +1,8 @@
+//header guard
+
+#if !defined(MASTER_H)
+#define MASTER_H
+
+int myFunction(int x, int y);
+
+#endif
