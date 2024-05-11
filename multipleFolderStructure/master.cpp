652477e20e79447e48b0d63a17ec0ca9ea5d3a05
puscianm <teranowa1995@gmail.com>
2024-05-11 21:06:40 +0200

    added project with multiple files

---
+++ multipleFolderStructure/master.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+int add(int var1, int var2);
+
+int main()
+{
+    std::cout << add(1,2);
+    return 0;
+}
---
+++ multipleFolderStructure/sourceAdd.cpp
@@ -0,0 +1,4 @@
+int add(int var1, int var2)
+{
+    return var1 + var2;
+}
